014a18046d0d96bb272be611d191cdebce72c713
Turkpenbaev Bauyrzhan <mr.turkpenbaev1997@gmail.com>
2021-04-16 18:22:34 +0600

    updated admin

--- app/app/settings/__init__.py
+++ app/app/settings/__init__.py
@@ -3,7 +3,8 @@ from .base import *
 
 if os.environ.get('MODE') == 'prod':
    from .prod import *
-elif os.environ.get('MODE') == 'heroku':
-   from .heroku import *
+   print('prod')
 else:
    from .dev import *
+   print('dev')
+   o
